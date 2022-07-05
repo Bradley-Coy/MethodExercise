23a201ceb28423ddca8e6586ace19c0bdff23acd
Bradley-Coy <bradleycoy95@gmail.com>
2022-07-05 08:51:29 -0700

    second exercise added

---
+++ Method_Exercise_2/Method_Exercise_2/Methods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Method_Exercise_2
+{
+    public class Methods
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        internal static void Add()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Method_Exercise_2/Method_Exercise_2/Program.cs
@@ -0,0 +1,8 @@
+﻿using Method_Exercise_2;
+
+var result_1 = Methods.Add(3, 6);
+
+var result_2 = Methods.Subtract(4, 2);
+
+Console.WriteLine($"The sum of three and six is equal to: {result_1}");
+Console.WriteLine(result_2);
