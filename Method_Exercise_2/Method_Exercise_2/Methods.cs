fd4f839ed566c35792b69a4250a7a16b9431ec7e
Bradley-Coy <bradleycoy95@gmail.com>
2022-07-05 09:41:43 -0700

    challenge

--- Method_Exercise_2/Method_Exercise_2/Methods.cs
+++ Method_Exercise_2/Method_Exercise_2/Methods.cs
@@ -3,15 +3,27 @@ namespace Method_Exercise_2
 {
     public class Methods
     {
-        public static int Add(int x, int y)
+        /*public static int Add(int x, int y)
         {
             return x + y;
-        }
+        }*/
         public static int Subtract(int x, int y)
         {
             return x - y;
         }
 
+        public static int Add(params int[] values)
+        {
+            int sum = 0;
+            foreach (int num in values)
+            {
+                sum += num;
+            }
+
+            return sum;
+        }
+
+       
         internal static void Add()
         {
             throw new NotImplementedException();
--- Method_Exercise_2/Method_Exercise_2/Program.cs
+++ Method_Exercise_2/Method_Exercise_2/Program.cs
@@ -1,8 +1,19 @@
 ﻿using Method_Exercise_2;
 
-var result_1 = Methods.Add(3, 6);
+/*var result_1 = Methods.Add(3, 6);
 
 var result_2 = Methods.Subtract(4, 2);
 
 Console.WriteLine($"The sum of three and six is equal to: {result_1}");
 Console.WriteLine(result_2);
+*/
+
+
+// Challenge
+
+var array = new int[] { 1, 6, 7 };
+
+var resolt = Methods.Add(1, 6, 7);
+
+Console.WriteLine(resolt);
+
