75214549a3d3089d7c0ee2a4a3e71ac4e8a9b1b0
Bradley-Coy <bradleycoy95@gmail.com>
2022-07-05 08:49:56 -0700

    first commit

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+Console.WriteLine("What is your favorite color?");
+var color = Console.ReadLine)();
+Console.WriteLine("What is your favorite animal?");
+var animal = Console.ReadLine();
+Console.WriteLine("What is your favorite band?");
