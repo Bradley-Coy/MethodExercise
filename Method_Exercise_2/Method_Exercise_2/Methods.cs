e255878b36bfc0a3242d1dbc81c50661f43f4088
Bradley-Coy <bradleycoy95@gmail.com>
2022-08-29 15:31:37 -0700

    added exercise 2

--- Method_Exercise_2/Method_Exercise_2/Methods.cs
+++ Method_Exercise_2/Method_Exercise_2/Methods.cs
@@ -3,16 +3,16 @@ namespace Method_Exercise_2
 {
     public class Methods
     {
-        /*public static int Add(int x, int y)
+        public static int Add_1(int x, int y)
         {
             return x + y;
-        }*/
+        }
         public static int Subtract(int x, int y)
         {
             return x - y;
         }
 
-        public static int Add(params int[] values)
+        public static int Add_2(params int[] values)
         {
             int sum = 0;
             foreach (int num in values)
@@ -23,6 +23,20 @@ namespace Method_Exercise_2
             return sum;
         }
 
+        public static int Mult(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
+        public static int Mod(int x, int y)
+        {
+            return x % y;
+        }
        
         internal static void Add()
         {
